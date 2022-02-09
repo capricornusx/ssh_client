0337cc5bb6075f226a2585e4c9fee97c132ebf93
Dmitry Dubinin <4762973+capricornusx@users.noreply.github.com>
2022-02-09 14:33:38 +0300

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,21 @@
+# General information about the project.
+project = 'all energy'
+copyright = '2022, all energy community'
+language = 'ru'
+today = ''
+today_fmt = '%Y-%m-%d'
+
+html_show_sphinx = False
+
+
+# Генерация в формат ePub
+epub_language = 'ru'
+
+# How to display URL addresses: 'footnote', 'no', or 'inline'.
+epub_show_urls = 'inline'
+
+# Bibliographic Dublin Core info.
+epub_title = 'all energy'
+epub_author = 'all energy community'
+epub_publisher = 'all energy community'
+epub_copyright = '2022, all energy community'
